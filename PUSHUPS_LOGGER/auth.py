75789b55ed4932ea9caa51850f6ff44507fea89c
murtazamultan-10p <murtaza.multan@10pearls.com>
2023-04-27 12:08:38 +0500

    flask: add templates

--- PUSHUPS_LOGGER/auth.py
+++ PUSHUPS_LOGGER/auth.py
@@ -1,16 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/signup")
 def signup():
-    return "signup page"
+    return render_template("signup.html")
+
 
 
 @auth.route("/signin")
 def signin():
-    return "signin page"
+    return render_template("signin.html")
 
 
 @auth.route("/signout")
