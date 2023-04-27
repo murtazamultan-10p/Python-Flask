cb614a97410a1e1be6e4b59d415e6f8eaf799194
murtazamultan-10p <murtaza.multan@10pearls.com>
2023-04-27 16:43:57 +0500

    flask: add register & login func with page redirect

--- PUSHUPS_LOGGER/auth.py
+++ PUSHUPS_LOGGER/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
@@ -7,12 +7,19 @@ auth = Blueprint("auth", __name__)
 def signup():
     return render_template("signup.html")
 
-
+@auth.route("/signup", methods=["POST"])
+def signup_post():
+    print(list(request.form.values()))
+    return redirect(url_for("auth.signin"))
 
 @auth.route("/signin")
 def signin():
     return render_template("signin.html")
 
+@auth.route("/signin", methods=["POST"])
+def signin_post():
+    print(list(request.form.values()))
+    return redirect(url_for("main.profile"))
 
 @auth.route("/signout")
 def signout():
